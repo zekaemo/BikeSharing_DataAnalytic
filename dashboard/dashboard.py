97e552ffa63b5d8e850a236363d8b8550a45e1c4
zekaemo <127576177+zekaemo@users.noreply.github.com>
2024-10-10 20:03:50 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -18,33 +18,31 @@ st.markdown('''
 df_day = pd.read_csv('dashboard/day.csv')
 df_hour = pd.read_csv('dashboard/hour.csv')
 
-# Filtering berdasarkan tanggal
-st.sidebar.header("Filter Data")
-start_date = st.sidebar.date_input("Pilih tanggal mulai", df_day['dteday'].min())
-end_date = st.sidebar.date_input("Pilih tanggal akhir", df_day['dteday'].max())
+# Mengubah kolom 'dteday' menjadi datetime
 df_day['dteday'] = pd.to_datetime(df_day['dteday'])
 df_hour['dteday'] = pd.to_datetime(df_hour['dteday'])
 
-# Filter berdasarkan range tanggal
-filtered_data_day = df_day[(df_day['dteday'] >= pd.to_datetime(start_date)) & (df_day['dteday'] <= pd.to_datetime(end_date))]
-filtered_data_hour = df_hour[(df_hour['dteday'] >= pd.to_datetime(start_date)) & (df_hour['dteday'] <= pd.to_datetime(end_date))]
-
-# Filtering by Season
+# Sidebar untuk Filtering berdasarkan musim dan cuaca
+st.sidebar.header("Filter Data")
 season_mapping = {1: 'Semi', 2: 'Panas', 3: 'Gugur', 4: 'Salju'}
 df_day['season'] = df_day['season'].map(season_mapping)
-season = st.sidebar.multiselect("Pilih Musim", options=df_day['season'].unique())
-if season:
-    filtered_data_day = filtered_data_day[filtered_data_day['season'].isin(season)]
 
-# Filtering by Weather
+season = st.sidebar.multiselect("Pilih Musim", options=df_day['season'].unique())
+
 weather_mapping = {1: 'Cerah', 2: 'Berkabut', 3: 'Hujan Salju Ringan', 4: 'Hujan Salju Lebat'}
 df_day['weathersit'] = df_day['weathersit'].map(weather_mapping)
 weather = st.sidebar.multiselect("Pilih Kondisi Cuaca", options=df_day['weathersit'].unique())
+
+# Filtering data berdasarkan musim dan cuaca
+filtered_data_day = df_day.copy()
+if season:
+    filtered_data_day = filtered_data_day[filtered_data_day['season'].isin(season)]
+
 if weather:
     filtered_data_day = filtered_data_day[filtered_data_day['weathersit'].isin(weather)]
 
+# Analisis Berdasarkan Hari
 st.header('Analisis Berdasarkan Hari')
-# Mengelompokkan data berdasarkan 'weekday' untuk mendapatkan total penyewaan
 weekday_mapping = {0: 'Minggu', 1: 'Senin', 2: 'Selasa', 3: 'Rabu', 4: 'Kamis', 5: 'Jumat', 6: 'Sabtu'}
 filtered_data_day['weekday'] = filtered_data_day['weekday'].map(weekday_mapping)
 weekday_rentals = filtered_data_day.groupby('weekday', observed=True)['cnt'].sum().reset_index()
@@ -59,7 +57,7 @@ st.pyplot(fig)
 
 # Filtering berdasarkan jam
 st.header('Analisis Berdasarkan Jam')
-hourly_rentals = filtered_data_hour.groupby('hr')['cnt'].sum().reset_index()
+hourly_rentals = df_hour.groupby('hr')['cnt'].sum().reset_index()
 
 # Visualisasi penyewaan sepeda berdasarkan jam
 st.subheader('Total Penyewaan Sepeda Berdasarkan Jam')
@@ -94,18 +92,3 @@ plt.xlabel('Kondisi Cuaca')
 plt.ylabel('Rata-rata Penyewaan Sepeda')
 plt.xticks(rotation=45)
 st.pyplot(fig)
-
-st.sidebar.header('Filter Tipe Pengguna')
-user_type = st.sidebar.radio('Pilih Tipe Pengguna', ['Semua', 'Registered', 'Casual'])
-if user_type == 'Registered':
-    user_rentals = filtered_data_day[['registered']].sum().reset_index()
-    user_rentals.columns = ['User Type', 'Total Rentals']
-elif user_type == 'Casual':
-    user_rentals = filtered_data_day[['casual']].sum().reset_index()
-    user_rentals.columns = ['User Type', 'Total Rentals']
-else:
-    user_rentals = filtered_data_day[['registered', 'casual']].sum().reset_index()
-    user_rentals.columns = ['User Type', 'Total Rentals']
-
-st.subheader(f'Jumlah Penyewaan untuk Tipe Pengguna: {user_type}')
-st.write(user_rentals)
